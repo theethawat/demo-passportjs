900e2fef33c775c23389b6cd3e9c316a82ee9c9f
Theethawat Savastham <5910110150@psu.ac.th>
2021-06-10 06:35:35 +0700

    Initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { Heading } from "@chakra-ui/react";
+import TDCLogo from "../Assets/TDC Logo black.png";
+
+const AppNavBar = () => {
+  return (
+    <div className='w-full bg-shamrock-100 py-2'>
+      <div className='container mx-auto px-4'>
+        <div className='flex'>
+          {/* <img className='h-8 ' src={TDCLogo} alt='The Duck Creator Logo' /> */}
+          <h2 className='text-2xl font-semibold text-white font-sans ml-4'>
+            {" "}
+            TDC - เดอะดักส์ครีเอเตอร์
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppNavBar;
